ee02b68f1de9c41dfe07a488c413e4ccdcb9efcc
Ayk <brutalmind99@gmail.com>
2024-10-02 22:52:00 -0400

    Fix buttons to use the primary button components

--- resources/js/Pages/Scraper.jsx
+++ resources/js/Pages/Scraper.jsx
@@ -2,26 +2,29 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
 import NavLink from '@/Components/NavLink';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGears } from '@fortawesome/free-solid-svg-icons';
+import { faGears, faPlus } from '@fortawesome/free-solid-svg-icons';
 import PlusButton from '@/Components/PlusButton';
+import PrimaryButton from '@/Components/PrimaryButton';
+import { Link } from '@nextui-org/react';
+
 
 export default function Scraper() {
     return (
         <AuthenticatedLayout
             header={
-                <div className='flex gap-x-8 relative'>
+                <div className='flex gap-x-8 justify-between relative'>
                     <h2 className="text-xl font-semibold float-start leading-tight text-gray-800">
                     <FontAwesomeIcon icon={faGears} className="text-primary-500 mr-2" />    Data Collected 
                     </h2>
 
-                    <NavLink
+                    <Link
                         href={route('register.website')}
-                        active={route().current('register.website')}
-                        className='float-right absolute right-0'
+                       
                     >
-                    <PlusButton classname="flex items-center justify-center w-5 h-5 "/>
-                    Register
-                    </NavLink>
+                    {/* <PlusButton classname="flex items-center justify-center w-5 h-5 "/>
+                    Register */}
+                    <PrimaryButton className='lower-case' startContent={<FontAwesomeIcon icon={faPlus}/>}>Register</PrimaryButton>
+                    </Link>
                 </div>
             }
         >
