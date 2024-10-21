ac146c50cb13e3ca9f6d47ec13f2bc6e76dc7628
Lit-23 <rommelmarquez143@gmail.com>
2024-10-20 20:44:37 -0400

    minor

--- resources/js/Pages/Scrape.jsx
+++ resources/js/Pages/Scrape.jsx
@@ -61,7 +61,7 @@ export default function Scrape({ registered_products }) {
                                     </div>
                                 </div>
 
-                                {/* <div className="">
+                                <div className="">
                                     <label for="country" className="block text-sm font-medium leading-6 text-gray-900">Register Product</label>
                                     <div className="mt-2">
                                         <select onChange={(e)=>handleProductChange(e.target.value)} id="country" name="country" autoComplete="country-name" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
@@ -71,7 +71,7 @@ export default function Scrape({ registered_products }) {
                                             <option value="3">Mexico</option>
                                         </select>
                                     </div>
-                                </div> */}
+                                </div>
                                 {/* <div>
                                     <SelectAutocomplete
                                         items={registered_products}
